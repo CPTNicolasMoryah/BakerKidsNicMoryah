012d67bb47e3ee0530e0ee2b0653bfafd2e4ca64
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2020-01-13 17:55:19 -0500

    Update level2_screen.lua

--- level2_screen.lua
+++ level2_screen.lua
@@ -275,8 +275,6 @@ local function BakingPowder( )
     if (BakingPowder1.x >= display.contentCenterX) then
        BakingPowder1TextField:resizeFontToFitHeight()
        BakingPowder2TextField:resizeFontToFitHeight()
-       BakingPowder1TextField:resizeFontToFitWidth()
-       BakingPowder2TextField:resizeFontToFitWidth()
        timer.cancel(bakingpowdertimer)       
     else
         bakingpowdertimer = timer.performWithDelay(-0.1,BakingPowder)  
@@ -299,8 +297,6 @@ local function BakingSoda( )
     if (BakingSoda1.x >= display.contentCenterX)then
         BakingSoda1TextField:resizeFontToFitHeight()
         BakingSoda2TextField:resizeFontToFitHeight()
-        BakingSoda1TextField:resizeFontToFitWidth()
-        BakingSoda2TextField:resizeFontToFitWidth()
         timer.cancel(BakingSodaTimer)
     else
         BakingSodaTimer = timer.performWithDelay(-0.01, BakingSoda)        
@@ -317,7 +313,6 @@ local function Butter1( )
     if(Butter.x == display.contentCenterX)then
         timer.cancel(ButterTimer)
         ButterTextField:resizeFontToFitHeight()
-        ButterTextField:resizeFontToFitWidth() 
     else
         ButterTimer = timer.performWithDelay(-0.1, Butter1)
     end
@@ -338,7 +333,6 @@ local function Eggs( )
     if(Eggs1.x == display.contentCenterX)then
         timer.cancel(EggsTimer)
         Eggs1TextField:resizeFontToFitHeight()
-        Eggs1TextField:resizeFontToFitWidth()
     else
         EggsTimer = timer.performWithDelay(-0.3,Eggs)
     end
@@ -354,7 +348,6 @@ local function Flour1( )
     if(Flour.x == display.contentCenterX)then
         timer.cancel(FlourTimer)
         FlourTextField:resizeFontToFitHeight()
-        FlourTextField:resizeFontToFitWidth()
     else
         FlourTimer = timer.performWithDelay(-0.1,Flour1)
     end
@@ -371,7 +364,6 @@ local function Milk1( )
     if(Milk.x <= display.contentCenterX)then
         timer.cancel(MilkTimer)
         MilkTextField:resizeFontToFitHeight()
-        MilkTextField:resizeFontToFitWidth()
     else
         MilkTimer = timer.performWithDelay(-0.23, Milk1)
     end
@@ -387,7 +379,7 @@ local function Salt1( )
     if(Salt.x >= display.contentCenterX)then
         timer.cancel(SaltTimer)
         SaltTextField:resizeFontToFitHeight()
-        SaltTextField:resizeFontToFitWidth()
+
     else
         SaltTimer = timer.performWithDelay(-0.3, Salt1)
     end
@@ -403,7 +395,7 @@ local function Sugar1( )
     if(Sugar.x == display.contentCenterX)then
         timer.cancel(SugarTimer)
         SugarTextField:resizeFontToFitHeight()
-        SugarTextField:resizeFontToFitWidth()
+
     else
         SugarTimer = timer.performWithDelay(-0.3, Sugar1)
     end
